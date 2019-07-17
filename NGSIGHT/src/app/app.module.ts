253f0115afd29fe701d2733e6b9d80322313c6de
hosamelgendy100 <hosamelgendy100@yahoo.com>
2019-07-17 17:15:43 +0300

    setup

--- NGSIGHT/src/app/app.module.ts
+++ NGSIGHT/src/app/app.module.ts
@@ -1,27 +1,41 @@
+import { AlertifyService } from './services/alertify.service';
+import { environment } from './../environments/environment.prod';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent, NavbarComponent, SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CollapseModule.forRoot(), BsDropdownModule.forRoot(),
-    FormsModule, HttpClientModule, ReactiveFormsModule
+    FormsModule, HttpClientModule, ReactiveFormsModule,
+    AngularFirestoreModule, AngularFireAuthModule, AngularFireStorageModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    BrowserAnimationsModule, ToastrModule.forRoot(), AngularFireDatabaseModule,
   ],
-  providers: [],
+  providers: [AlertifyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
